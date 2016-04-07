da06fea0a0d3c633e34ed44f0106bb60f7a850c2
gbraccialli <gbraccialli@gmail.com>
2016-04-07 03:22:45 -0300

    samples calls

---
+++ scripts/samples.sh
@@ -0,0 +1,12 @@
+java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar.jar --confDir=/Users/gbraccialli/Documents/workspace/hdfs-cli/conf --verbose=true
+
+java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar.jar --confDir=/Users/gbraccialli/Documents/workspace/hdfs-cli/conf --verbose=true
+
+java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar.jar \
+  --confDir=/Users/gbraccialli/Documents/workspace/hdfs-cli/conf \
+  --path=/ \
+  --maxLevelThreshold=5  \
+  --minSizeThreshold=50000  \
+  --showFiles=true   \
+  --verbose=true > out.json  
+
