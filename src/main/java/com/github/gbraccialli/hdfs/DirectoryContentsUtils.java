448018475ca92da1a9dade2896c483f5c6b0804b
gbraccialli <gbraccialli@gmail.com>
2016-04-07 03:22:25 -0300

    main class

--- src/main/java/com/github/gbraccialli/hdfs/DirectoryContentsUtils.java
+++ src/main/java/com/github/gbraccialli/hdfs/DirectoryContentsUtils.java
@@ -1,13 +1,81 @@
 package com.github.gbraccialli.hdfs;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.apache.hadoop.fs.ContentSummary;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class DirectoryContentsUtils {
+
+	public static void main(String[] args) throws Exception {
+
+		String confDir = "/etc/hadoop/conf";
+		String path = "/";
+		int maxLevelThreshold = -1;
+		long minSizeThreshold = -1;
+		boolean showFiles = false;
+		boolean verbose = false;
+		
+		for (String arg : args){
+			if (arg.startsWith("--confDir=")){
+				confDir = arg.split("=")[1];
+			}
+			if (arg.startsWith("--path=")){
+				path = arg.split("=")[1];
+			}
+			if (arg.startsWith("--maxLevelThreshold=")){
+				maxLevelThreshold = Integer.parseInt(arg.split("=")[1]);
+			}
+			if (arg.startsWith("--minSizeThreshold=")){
+				minSizeThreshold = Long.parseLong(arg.split("=")[1]);
+			}
+			if (arg.startsWith("--showFiles=")){
+				showFiles = Boolean.parseBoolean(arg.split("=")[1]);
+			}
+			if (arg.startsWith("--verbose=")){
+				verbose = Boolean.parseBoolean(arg.split("=")[1]);
+			}
+		}
+
+		Date dateStart = new Date();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+		FileSystem hdfs = HDFSConfigUtils.loadConfigsAndGetFileSystem(confDir);
+		Path hdfsPath = new Path(path);
+
+		if (verbose){
+			System.err.println("confDir=" + confDir);
+			System.err.println("path=" + path);
+			System.err.println("maxLevelThreshold=" + maxLevelThreshold);
+			System.err.println("minSizeThreshold=" + minSizeThreshold);
+			System.err.println("showFiles=" + showFiles);
+			System.err.println("verbose=" + verbose);
+			System.err.println("uri=" + hdfs.getUri().toString());
+			System.err.println("start at:" + dateFormat.format(dateStart));
+		}
+
+		System.out.println(directoryInfoToJson(DirectoryContentsUtils.listContents(hdfs,hdfsPath,0,maxLevelThreshold,minSizeThreshold,showFiles,verbose)));
+
+		if (verbose){
+			Date dateEnd = new Date();
+			System.err.println();
+			System.err.println("end at:" + dateFormat.format(dateEnd));
+			System.err.println("elapsed time: " + (dateEnd.getTime() - dateStart.getTime()) / 1000.0 + " seconds") ;
+		}
+	}
+
+	private static String directoryInfoToJson(PathInfo directoryInfo) {
+		Gson gson = new GsonBuilder().create();
+		return gson.toJson(directoryInfo);
+	}
 
 	public static PathInfo listContents(FileSystem hdfs, Path path) throws Exception{
 		return listContents(hdfs,path,0,-1,-1,false,false);
@@ -40,10 +108,16 @@ public class DirectoryContentsUtils {
 		}
 
 		if (dir.isDirectory()){
-			if (maxLevelThreshold > -1 && currentLevel > maxLevelThreshold){
+			if (maxLevelThreshold > -1 && currentLevel >= maxLevelThreshold){
 				dir.setMessage("Drill down stopped due to maxLevelThreshold achieved (" + maxLevelThreshold + ")");
+				if (verbose){
+					System.err.println("Drill down stopped due to maxLevelThreshold achieved (" + maxLevelThreshold + ")");
+				}
 			}else if (minSizeThreshold > -1 && totalLength < minSizeThreshold){
 				dir.setMessage("Drill down stopped due to minSizeThreshold achieved (" + minSizeThreshold + ")");
+				if (verbose){
+					System.err.println("Drill down stopped due to minSizeThreshold achieved (" + minSizeThreshold + ")");
+				}
 			}else{
 				long subDirsLength=0;
 				long subDirsSpaceConsumed=0;
