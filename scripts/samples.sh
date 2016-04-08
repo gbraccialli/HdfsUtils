52dbcd0e5322f73e99ce8e4e01252664cf7f4596
gbraccialli <gbraccialli@gmail.com>
2016-04-08 03:06:43 -0300

    new version

--- scripts/samples.sh
+++ scripts/samples.sh
@@ -1,12 +1,18 @@
-java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar.jar --confDir=/Users/gbraccialli/Documents/workspace/hdfs-cli/conf --verbose=true
+java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar \
+  --path=/ \
+  --maxLevelThreshold=-1  \
+  --minSizeThreshold=-1  \
+  --showFiles=false   \
+  --verbose=true > out.json  
 
-java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar.jar --confDir=/Users/gbraccialli/Documents/workspace/hdfs-cli/conf --verbose=true
 
-java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar.jar \
-  --confDir=/Users/gbraccialli/Documents/workspace/hdfs-cli/conf \
+java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar \
   --path=/ \
   --maxLevelThreshold=5  \
   --minSizeThreshold=50000  \
   --showFiles=false   \
   --verbose=true > out.json  
 
+java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar --confDir=/Users/gbraccialli/Documents/workspace/hdfs-cli/conf --verbose=true
+
+java -jar target/gbraccialli-hdfs-utils-with-dependencies.jar --confDir=/Users/gbraccialli/Documents/workspace/hdfs-cli/conf --verbose=true
