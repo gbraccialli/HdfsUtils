4aa4e421fc3ed5528e903703df88531ce03d7f73
Guilherme Braccialli <gbraccialli@hotmail.com>
2017-07-28 22:02:02 +0100

    support to s3

--- src/main/java/com/github/gbraccialli/hdfs/HDFSConfigUtils.java
+++ src/main/java/com/github/gbraccialli/hdfs/HDFSConfigUtils.java
@@ -5,16 +5,21 @@ import java.io.File;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import java.net.URI;
 
 public class HDFSConfigUtils {
 
 	private static final String[] HADOOP_CONF_FILES = {"core-site.xml", "hdfs-site.xml"};
 
 	public static FileSystem loadConfigsAndGetFileSystem(String hadoopConfDirProp) throws Exception{
-		return loadConfigsAndGetFileSystem(hadoopConfDirProp, null);
+		return loadConfigsAndGetFileSystem(hadoopConfDirProp, null, null);
 	}
 
-	public static FileSystem loadConfigsAndGetFileSystem(String hadoopConfDirProp, String doAs) throws Exception{
+	public static FileSystem loadConfigsAndGetFileSystem(String hadoopConfDirProp, String uri) throws Exception{
+		return loadConfigsAndGetFileSystem(hadoopConfDirProp, uri, null);
+	}
+
+	public static FileSystem loadConfigsAndGetFileSystem(String hadoopConfDirProp, String uri, String doAs) throws Exception{
 
 		if (doAs != null){
 			System.setProperty("HADOOP_USER_NAME", doAs);
@@ -36,7 +41,12 @@ public class HDFSConfigUtils {
 		}
 
 		try {
-			hdfs = FileSystem.get(config);
+			URI fsURI;
+			if (uri != null && uri.length() > 0){
+				hdfs = FileSystem.get(new URI(uri),config);
+			}else{
+				hdfs = FileSystem.get(config);
+			}
 			if (doAs != null){
 				hdfs = FileSystem.get(hdfs.getUri(),config, doAs);
 			}
